83991d058c563e0caae794425c678f341ffac3a8
EmiliaVatslavovna <emilia.podgaiskaya@mail.ru>
2023-08-08 15:45:06 +0300

    feat: reverse integer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let reverse = 0, num, negative = n;
+    while (n !== 0) {
+        num = n % 10;
+        reverse = reverse * 10 + num;
+        n = parseInt(n / 10);
+      }
+    if (negative < 0) return reverse * (-1);
+    return reverse;  
 }
